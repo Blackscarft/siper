d03fdf37c78ed853909aee8b9a054026ab0c6081
Agus Nugroho <agusnugroho.blackscarft@gmail.com>
2026-05-15 14:25:09 +0700

    Add Read me

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,10 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2025 &copy; JobShare</p>
+            <p><sup>©</sup> 2026 <a href="https://www.instagram.com/agus_nugrh/">Suga Ho</a>. Free for non-commercial use with clear attribution to the author.</p>
         </div>
         <div class="float-end">
-            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+            <p>Template Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
                 by <a href="https://saugi.me">JobShare</a></p>
         </div>
     </div>
